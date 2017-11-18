6ad03ff0d57752826f4444b4f56c7c3228c5cb77
lyrahgames <markuspawellek@gmail.com>
2017-11-18 20:40:38 +0100

    Link libraries 'OpenGL' and 'GLUT' to project

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,25 @@
+#include <GL/glut.h>
 #include <Eigen/Dense>
 #include <iostream>
 
-using Eigen::MatrixXd;
+void renderScene() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-int main() {
-  MatrixXd m(2, 2);
-  m(0, 0) = 3;
-  m(1, 0) = 2.5;
-  m(0, 1) = -1;
-  m(1, 1) = m(1, 0) + m(0, 1);
-  std::cout << m << std::endl;
+  glBegin(GL_TRIANGLES);
+  glVertex3f(-0.5, -0.5, 0.0);
+  glVertex3f(0.5, 0.0, 0.0);
+  glVertex3f(0.0, 0.5, 0.0);
+  glEnd();
+
+  glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(320, 320);
+  glutCreateWindow("GLUT Tutorial");
+  glutDisplayFunc(renderScene);
+  glutMainLoop();
 }
